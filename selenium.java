17783f395a70dd2af0c6297d2ff1978c1b9a7b63
Sumanth1431 <uniquesumanth143@gmail.com>
2024-09-20 09:58:36 +0530

    code updated

--- selenium.java
+++ selenium.java
@@ -12,7 +12,7 @@ public class FirstScript {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
+        //hello
         driver.getTitle();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
